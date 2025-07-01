a179d2b6007b8a5031f37549e25cfcfa8e783d42
sairamavvaru <sairamavvaru203@gmail.com>
2025-07-01 18:16:32 +0530

    Updated EC2 instance config in main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/16"]
   }
 
   ingress {
